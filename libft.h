f7927654a06e2a52bbd7e4cb6340ee9ed8ab0f1e
Soongood <46536006+Soongood@users.noreply.github.com>
2019-09-16 21:32:10 +0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -21,6 +21,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 void	ft_putchar(char c);
